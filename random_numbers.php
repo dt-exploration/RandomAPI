133a6c38754d59f7e4fad8c84da439d4b9e7a904
Dusan <masinacdt@gmail.com>
2019-02-08 12:06:30 +0100

    -PSR edit

--- random_numbers.php
+++ random_numbers.php
@@ -10,7 +10,8 @@
   Koliko brojeva zelis da vidis:<br>
   <input type="text" name="howmany"><br><br>
   <input type="submit" value="Lets go" name="submit">
-</form>
+  </form>
+
 <?php
 //The URL for invoking the API is https://api.random.org/json-rpc/1/invoke
 /*
@@ -27,43 +28,47 @@
     "id": 42
 */
 if (isset($_POST["submit"])) {
+
    $min=$_POST["min"];
    $max=$_POST["max"];
    $how_many=$_POST["howmany"];
 
-$request=array("jsonrpc"=> "2.0", "method"=> "generateIntegers", "params" => array(
-"apiKey"=> "d116ab95-8579-4196-9426-a46a187e4a8c",
-"n"=> $how_many,
-"min"=> $min,
-"max"=> $max,
-"replacement"=> true) , "id"=>42);
-$data=json_encode($request);
+    $request=array("jsonrpc"=> "2.0", "method"=> "generateIntegers", "params" =>
+                array(
+                    "apiKey"=> "d116ab95-8579-4196-9426-a46a187e4a8c",
+                     "n"=> $how_many,
+                     "min"=> $min,
+                     "max"=> $max,
+                     "replacement"=> true) ,
+                   "id"=>42);
 
-$url="https://api.random.org/json-rpc/1/invoke";
-$ch=curl_init();
+    $data=json_encode($request);
 
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-           'Content-Type: application/json',
-           'Content-Length: ' . strlen($data))
-            );
-$result=curl_exec($ch);
+    $url="https://api.random.org/json-rpc/1/invoke";
+    $ch=curl_init();
+
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                   'Content-Type: application/json',
+                   'Content-Length: ' . strlen($data))
+               );
+    $result=curl_exec($ch);
 
 //j_decoded kastovano kao niz jer ako je string $result prazan, sto je cesto
 //slucaj, onda json decode vraca null i
 //prelomi program u foreachu sa greskom
 
-$j_decoded=(array) json_decode($result, true);
-$a=$j_decoded["result"];
-$b=$a["random"];
-$c=$b["data"];
+    $j_decoded=(array) json_decode($result, true);
+    $a=$j_decoded["result"];
+    $b=$a["random"];
+    $c=$b["data"];
 
-foreach ($c as $value) {
-    echo "$value<br>";
-}
+    foreach ($c as $value) {
+        echo "$value<br>";
+    }
 
 /*
 $options = array(
@@ -81,6 +86,7 @@ $response = json_decode( $result );
 var_dump($response);
 */
 }
+
 ?>
 </body>
 </html>
